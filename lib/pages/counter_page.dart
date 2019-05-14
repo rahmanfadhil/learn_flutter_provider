c0577e423966ee2232ea08f88e4f02e936b08ae4
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-05-14 07:16:17 +0700

    Move counter manager to counter page

--- lib/pages/counter_page.dart
+++ lib/pages/counter_page.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:learn_flutter_provider/widgets/shared/main_drawer.dart';
-import 'package:learn_flutter_provider/widgets/counter/counter_manager.dart';
+import 'package:learn_flutter_provider/blocs/counter_bloc.dart';
 
 class CounterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final CounterBloc counterBloc = Provider.of<CounterBloc>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Counter'),
       ),
       drawer: MainDrawer(),
-      body: CounterManager(),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(counterBloc.counter.toString()),
+            RaisedButton(
+              child: Text('Increase'),
+              onPressed: () => counterBloc.counter++,
+            ),
+            RaisedButton(
+              child: Text('Decrease'),
+              onPressed: () => counterBloc.counter--,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
